40566140f59eba8bec5ee1480ba123674d71785e
Uwe Ullrich <mail@uullrich.com>
2022-04-21 15:29:33 +0200

    Mobile view change

--- src/components/AboutMe.tsx
+++ src/components/AboutMe.tsx
@@ -31,6 +31,22 @@ const Content = styled.div`
     }
 `;
 
+const Section = styled.section`
+    display: flex;
+    flex-direction: column;
+    justify-content: baseline;
+    max-width: 700px;
+    min-width: 500px;
+    gap: 40px;
+
+    ${media.lessThan("small")`
+        padding-left: 20px;
+        padding-right: 20px;
+        min-width: 0;
+        width: 100%;
+    `};
+`;
+
 const Cards = styled.div``;
 
 const Card = styled.div`
@@ -62,51 +78,53 @@ const Description = styled.div`
 const AboutMe: React.FC<Props> = ({}) => {
     return (
         <Content>
-            <Card>
+            <Section>
+                <Card>
+                    <Header>
+                        <h1>About Me</h1>
+                        <Line />
+                        <StaticImage 
+                            id='me' 
+                            src="../images/me.jpg" 
+                            className='outerWrapper'
+                            imgClassName='img'
+                            alt={'Image from Uwe Ullrich'} />
+                    </Header>
+                    <Description>
+                    Hi! <br/>
+                    My name is Uwe Ullrich and I live in Kirchheim unter Teck. 
+                    I am a freelance software and web engineer dedicated to develop across the fullstack. 
+                    On the one hand I like to create web frontends with the whole <strong>React</strong> "ecosystem", but on the other hand it is also a joy for me to develop <strong>Node.js</strong>.
+                    <br/><br/>
+                    You can always ask me for hire. If I am available, we will hopefully work together. Let me support you with one or more of these topics:
+                    <br/><br/>
+                    <ul>
+                        <li>Frontend development with JavaScript / TypeScript</li>
+                        <li>Backend development with Node.js</li>
+                        <li>Cross plattform development with the Qt Framework in C++</li>
+                        <li>SAP ABAP OO development</li>
+                    </ul>
+                    </Description>
+                </Card>
+                <Card>
                 <Header>
-                    <h1>About Me</h1>
+                    <h1>How my story began...</h1>
                     <Line />
-                    <StaticImage 
-                        id='me' 
-                        src="../images/me.jpg" 
-                        className='outerWrapper'
-                        imgClassName='img'
-                        alt={'Image from Uwe Ullrich'} />
                 </Header>
-                <Description>
-                Hi! <br/>
-                My name is Uwe Ullrich and I live in Kirchheim unter Teck. 
-                I am a freelance software and web engineer dedicated to develop across the fullstack. 
-                On the one hand I like to create web frontends with the whole <strong>React</strong> "ecosystem", but on the other hand it is also a joy for me to develop <strong>Node.js</strong>.
-                <br/><br/>
-                You can always ask me for hire. If I am available, we will hopefully work together. Let me support you with one or more of these topics:
-                <br/><br/>
-                <ul>
-                    <li>Frontend development with JavaScript / TypeScript</li>
-                    <li>Backend development with Node.js</li>
-                    <li>Cross plattform development with the Qt Framework in C++</li>
-                    <li>SAP ABAP OO development</li>
-                </ul>
+                <Description style={{marginBottom: '20px'}}>
+                    My programming journey started at the age of 14. 
+                    Most of the time I hung around with my friends in Teamspeak.
+                    There was this cool programming dude who was able to create a tool to control the Teamspeak server.
+                    He showed me how it worked under the hood and I was on fire. 
+                    That's why I would say my first programming language I got in touch with was Visual Basic 6.0. <br/><br/>
+
+                    Several years later I got the chance to make my german Abitur (graduation) with a strong focus on information technology.
+                    During that time I have learned programming with Java from scratch and the basics of information technology. 
+                    We have developed some funny games in class with a 3D Engine (jMonkey Engine).
+
                 </Description>
-            </Card>
-            <Card>
-            <Header>
-                <h1>How my story began...</h1>
-                <Line />
-            </Header>
-            <Description style={{marginBottom: '20px'}}>
-                My programming journey started at the age of 14. 
-                Most of the time I hung around with my friends in Teamspeak.
-                There was this cool programming dude who was able to create a tool to control the Teamspeak server.
-                He showed me how it worked under the hood and I was on fire. 
-                That's why I would say my first programming language I got in touch with was Visual Basic 6.0. <br/><br/>
-
-                Several years later I got the chance to make my german Abitur (graduation) with a strong focus on information technology.
-                During that time I have learned programming with Java from scratch and the basics of information technology. 
-                We have developed some funny games in class with a 3D Engine (jMonkey Engine).
-
-            </Description>
-            </Card>
+                </Card>
+            </Section>
         </Content>
     );
 };
